d4180ad70a4097840c6d7493cecfcce0ce3ef2ec
martinsmessias <messiasads2017@gmail.com>
2019-12-27 13:21:05 -0500

    Quase lá! Funcionando em 50%! Implementando as urls, templates e views.

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from core import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('get_query/<selected_dork>', views.get_query, name='get_query'),
+    path('search/<id>+<query>', views.search, name='search'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from core.models import Dork
 
 
 def index(request):
     dorks = Dork.objects.all()
     return render(request, 'index.html', {'dorks':dorks})
+
+def get_query(request, selected_dork):
+    dork = Dork.objects.get(id=selected_dork)
+    return render(request, 'search.html', {'dork': dork})
+
+
+def search(request, id, query):
+    google_url = 'https://www.google.com/search?q='
+    dork = Dork.objects.get(id=id)
+    new_url = google_url+dork.dork+str(query)
+    return redirect(new_url)
