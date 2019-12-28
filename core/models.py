a24aa87c715212b7212baa09f628e6aecbaab6e7
MartinsMessias <messiasads2017@gmail.com>
2019-12-28 12:14:38 -0500

    Correções de template

--- core/models.py
+++ core/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Dork(models.Model):
     QUERY = (('Yes', 'Yes'),('Not', 'Not'))
-    tag = models.CharField(max_length=250)
-    explanation = models.TextField()
-    dork = models.CharField(max_length=250)
+    tag = models.CharField(max_length=250,default='')
+    explanation = models.TextField(default='')
+    dork = models.CharField(max_length=250, default='')
     query = models.CharField(choices=QUERY, max_length=3, default='Yes')
 
     def __str__(self):
